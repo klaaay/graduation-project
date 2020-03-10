406ffdd52f3844c747ae3d43ccd99a2f80d01873
吴震 <wuzhen01@corp.netease.com>
2020-03-10 12:52:39 +0800

    update: 删除无用代码

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import Axios, { AxiosRequestConfig } from 'axios';
-import Cookies from 'js-cookie';
-
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
-import UploadPage from '@/pages/UploadPage';
 import Home from '@/pages/Home';
 import Detail from '@/pages/Detail';
 import AdminLogin from '@/pages/AdminLogin';
 import AdminHome from '@/pages/AdminHome';
 import AdminProjectDetail from '@/pages/AdminProjectDetail';
 import AdminCovert from '@/pages/AdminCovert';
-import { AuthProvider, useAuth } from '@/context/authContext';
+import { AuthProvider } from '@/context/authContext';
 
 function App() {
-  // Axios.defaults.headers.common['x-auth-token'] = Cookies.get('token');
-
   return (
     <BrowserRouter>
       <AuthProvider>
         <div className="App">
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/upload" component={UploadPage} />
             <Route exact path="/detail" component={Detail} />
             <Route path="/admin">
               <Switch>
